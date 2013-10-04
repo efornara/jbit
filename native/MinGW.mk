ce803cbe58a89a0b11be91299a4e0552f26d36b1
Emanuele Fornara <efornara@users.sourceforge.net>
2013-10-04 20:52:12 +0100

    development / release compilation flags

--- native/MinGW.mk
+++ native/MinGW.mk
@@ -1,7 +1,7 @@
 VPATH = .:../core
 
 CXX = i586-mingw32msvc-g++
-CXXFLAGS = -I../core -std=gnu++98 -fno-exceptions -fno-rtti -Wall -O2 -s
+CXXFLAGS = -I../core -std=gnu++98 -fno-exceptions -fno-rtti -Wall -O2 -fomit-frame-pointer -s
 
 OBJS = main.o stdout.o cpu.o asm.o symdefs.o
 EXE = jbit.exe
