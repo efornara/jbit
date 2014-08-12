95307eef83395b77c7f5c832323cd960a236b687
Emanuele Fornara <efornara@users.sourceforge.net>
2014-08-12 18:14:33 +0100

    win32: hello world

--- native/MinGW.mk
+++ native/MinGW.mk
@@ -1,15 +1,30 @@
+#
+# VARIABLES
+#
+
+include ../Version.defs
+
 VPATH = .:../core
 
 CXX = i586-mingw32msvc-g++
-CXXFLAGS = -I../core -std=gnu++98 -fno-exceptions -fno-rtti -Wall -O2 -fomit-frame-pointer -s
+CXXFLAGS = -I../core -std=gnu++98 -fno-exceptions -fno-rtti -Wall -O2 -fomit-frame-pointer -s -DJBIT_VERSION=\"${JBIT_VERSION}\"
+LDFLAGS = -mwindows
+LIBS =
 
 OBJS = main.o stdout.o cpu.o asm.o symdefs.o
 EXE = jbit.exe
 
-all: $(EXE)
+#
+# RULES
+#
+
+all: $(EXE) jbitw.exe
 
 $(EXE): $(OBJS)
-	$(CXX) $(CXXFLAGS) $(LDFLAGS) -o $(EXE) $(OBJS) $(LIBS)
+	$(CXX) $(CXXFLAGS) -o $(EXE) $(OBJS)
+
+jbitw.exe: win32.o
+	$(CXX) $(CXXFLAGS) $(LDFLAGS) -o jbitw.exe win32.o $(LIBS)
 
 clean:
-	$(RM) $(EXE) *.o
+	$(RM) *.exe *.o
---
+++ native/win32.cpp
@@ -0,0 +1,104 @@
+/*
+ * Copyright (C) 2014  Emanuele Fornara
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <windows.h>
+
+namespace {
+
+void create() {
+}
+
+void paint(HDC dc) {
+	const char *msg = "Hello, World!";
+	TextOut(dc, 10, 10, msg, strlen(msg));
+}
+
+} // namespace;
+
+static const char *szClassName = "jbit";
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+	switch (uMsg) {
+	case WM_CREATE:
+		create();
+		break;
+	case WM_PAINT: {
+		PAINTSTRUCT ps;
+		HDC hdc = BeginPaint(hWnd, &ps);
+		paint(hdc);
+		EndPaint(hWnd, &ps);
+		} break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, uMsg, wParam, lParam);
+		break;
+	}
+	return 0;
+}
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+  LPSTR lpCmdLine, int nCmdShow) {
+
+	WNDCLASSEX wcex;
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.style          = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc    = WndProc;
+	wcex.cbClsExtra     = 0;
+	wcex.cbWndExtra     = 0;
+	wcex.hInstance      = hInstance;
+	wcex.hIcon          = LoadIcon(hInstance, (char *)IDI_APPLICATION);
+	wcex.hCursor        = LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+	wcex.lpszMenuName   = NULL;
+	wcex.lpszClassName  = szClassName;
+	wcex.hIconSm        = LoadIcon(wcex.hInstance, (char *)IDI_APPLICATION);
+	RegisterClassEx(&wcex);
+
+	HWND hWnd = CreateWindow(
+		szClassName,
+		"JBit " JBIT_VERSION,
+		WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		200, 300,
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+	);
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0)) {
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return msg.wParam;
+}
