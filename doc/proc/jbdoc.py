7d1b135201b0c33e85f9789f5352ce45da6ac3e0
Emanuele Fornara <efornara@users.sourceforge.net>
2015-03-21 18:40:38 +0000

    jbdoc: html converter

--- doc/proc/jbdoc.py
+++ doc/proc/jbdoc.py
@@ -32,52 +32,88 @@ import collections
 import os
 import sys
 import textwrap
+import re
 
 PARLEN = 77
 
-Section = collections.namedtuple("Section", "kind content indexed")
+fallback_resources = {}
+
+fallback_resources['html_header.txt'] = """
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+<head>
+<title>JBDoc - __TITLE__</title>
+<meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
+<meta name="viewport" content="width=device-width" />
+<meta name="HandheldFriendly" content="true" />
+<style type="text/css">
+body { font-family: sans-serif; }
+.nav { font-size: 70%; }
+#links a { margin: 0em 0.5em; }
+</style>
+</head>
+<body>
+<p align="right"><a class="nav" href="#content">skip nav</a></p>
+"""
+
+fallback_resources['html_footer.txt'] = """
+<p><a class="nav" href="#top">top</a></p>
+</body>
+</html>
+"""
+
+def resource(res):
+	return fallback_resources[res][1:]
+
+def template_resource(res, subs):
+	s = resource(res)
+	for k, v in subs.iteritems():
+		s = s.replace('__' + k + '__', v)
+	return s
+
+Section = collections.namedtuple('Section', 'kind content indexed')
 
 class JBDoc:
-	basename = ""
+	basename = ''
 	sections = []
 	def __init__(self, filename):
 		def section(kind, content, indexed):
 			self.sections.append(Section(kind, content, indexed))
 		def parse_section(s, lineno):
-			if s.startswith("# "):
-				if s.endswith(" #\n"):
-					section("a", s[2:-3], True)
+			if s.startswith('# '):
+				if s.endswith(' #\n'):
+					section('a', s[2:-3], True)
 				else:
-					section("a", s[2:-1], False)
-			elif s.startswith("## "):
-				if s.endswith(" ##\n"):
-					section("b", s[3:-4], True)
+					section('a', s[2:-1], False)
+			elif s.startswith('## '):
+				if s.endswith(' ##\n'):
+					section('b', s[3:-4], True)
 				else:
-					section("b", s[3:-1], False)
-			elif s.startswith("### "):
-				section("i", s[4:-1], False)
-			elif s.startswith("    "):
+					section('b', s[3:-1], False)
+			elif s.startswith('### '):
+				section('i', s[4:-1], False)
+			elif s.startswith('    '):
 				lines = s.splitlines()
-				s = ""
+				s = ''
 				for l in lines:
-					s += l[4:] + "\n"
-				section("#", s, False)
+					s += l[4:] + '\n'
+				section('#', s, False)
 			else:
-				section("p", s[:-1].replace("\n", " "), False)
+				section('p', s[:-1].replace('\n', ' '), False)
 		def parse_file(f):
 			lineno = 0
-			s = ""
+			s = ''
 			for line in f:
 				lineno += 1
 				line = line.rstrip()
 				if len(line) == 0:
 					parse_section(s, lineno)
-					s = ""
+					s = ''
 				else:
-					s += line + "\n"
+					s += line + '\n'
 			if len(s) != 0:
 				parse_section(s)
-		with open(filename, "r") as f:
+		with open(filename, 'r') as f:
 			parse_file(f)
 		self.basename = os.path.basename(filename)
 
@@ -97,18 +133,18 @@ class TextPageConverter(PageConverter):
 		def ruler(content):
 			return '=' * len(content)
 		def out_header(kind, content):
-			if (kind == 'a'):
+			if kind == 'a':
 				self.outln(ruler(content))
 			self.outln(content)
 			self.outln(ruler(content))
 		def out_section(kind, content):
-			if (kind == '#'):
+			if kind == '#':
 				self.out(content)
-			elif (kind == 'a' or kind == 'b'):
+			elif kind == 'a' or kind == 'b':
 				out_header(kind, content)
-			elif (kind == 'p'):
+			elif kind == 'p':
 				self.outln(textwrap.fill(content, PARLEN))
-			elif (kind == 'i'):
+			elif kind == 'i':
 				self.out('* ')
 				self.outln(content)
 			self.outln('')
@@ -127,11 +163,73 @@ class DATPageConverter(PageConverter):
 		for s in self.doc.sections:
 			out_section(s.kind, s.content, s.indexed)
 
+class HTMLPageConverter(PageConverter):
+	title = 'Untitled'
+	def convert(self):
+		def escape(s):
+			s = re.sub('&', '&amp;', s)
+			s = re.sub('<', '&lt;', s)
+			s = re.sub('>', '&gt;', s)
+			return s
+		def out_toc(kind, content, indexed):
+			if not indexed:
+				return
+			self.out('<a class="nav" href="#id' + str(id[0]) + '">')
+			self.out(escape(content))
+			self.outln('</a><br />')
+			id[0] += 1
+		def out_header(kind, content, indexed):
+			c = escape(content)
+			if kind == 'a':
+				el = 'h1'
+			else:
+				el = 'h2'
+			attr = ''
+			if indexed:
+				if id[0] != 1:
+					self.outln('<p><a class="nav" href="#top">top</a></p>')
+				self.outln('<hr />')
+				attr = ' id="id' + str(id[0]) + '"'
+				id[0] += 1
+			self.outln('<' + el + attr + '>' + c + '</' + el + '>');
+		def out_section(kind, content, indexed):
+			if kind == '#':
+				self.outln('<pre>')
+				self.out(escape(content))
+				self.outln('</pre>')
+			elif kind == 'a' or kind == 'b':
+				out_header(type, content, indexed)
+			elif kind == 'p':
+				self.outln('<p>')
+				self.outln(escape(textwrap.fill(content, PARLEN)))
+				self.outln('</p>')
+			elif kind == 'i':
+				self.out('<h3>')
+				self.out(escape(content))
+				self.outln('</h3>')
+		if len(self.doc.sections) > 0:
+			s = self.doc.sections[0]
+			if s.kind == 'a':
+				self.title = escape(s.content)
+		self.out(template_resource('html_header.txt', { 'TITLE': self.title }))
+		self.outln('<p id="top">')
+		id = [1]
+		for s in self.doc.sections:
+			out_toc(s.kind, s.content, s.indexed)
+		self.outln('</p>');
+		self.outln('<p id="content">')
+		id = [1]
+		for s in self.doc.sections:
+			out_section(s.kind, s.content, s.indexed)
+		self.out(resource('html_footer.txt'))
+
 def convert(in_file_name, out_file_name):
 	if out_file_name.endswith('.txt'):
 		converter = TextPageConverter()
 	elif out_file_name.endswith('.dat'):
 		converter = DATPageConverter()
+	elif out_file_name.endswith('.html') or out_file_name.endswith('.htm'):
+		converter = HTMLPageConverter()
 	else:
 		raise StandardError('unrecognized output extension')
 	converter.doc = JBDoc(in_file_name)
@@ -139,7 +237,7 @@ def convert(in_file_name, out_file_name):
 	converter.convert()
 
 def usage():
-	print "usage: jbdoc.py input output"
+	print 'usage: jbdoc.py input output'
 	sys.exit(1)
 
 if len(sys.argv) != 3:
