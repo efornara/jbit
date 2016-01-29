c9037a80332d6a929bfc21af43c63d6d32287634
Emanuele Fornara <efornara@users.sourceforge.net>
2016-01-29 21:20:08 +0000

    added mkdroid.sh: android binary distribution script

---
+++ dists/android/mkdroid.sh
@@ -0,0 +1,33 @@
+#! /bin/sh
+
+if [ ! -f ../../Version.defs ] ; then
+	echo "please, run from dists/android."
+	exit 1
+fi
+
+# version dependent file names
+. ../../Version.defs
+ID=jbit-$JBIT_VERSION
+IN=../..
+OUT=tmp/bin
+
+# make empty tmp directory
+rm -rf tmp
+mkdir -p $OUT
+
+# make binaries (all combinations)
+( cd $IN/native ; for arch in arm x86 mips ; do
+for elf in pie nopie static ; do
+./MkAndroid.sh $arch $elf
+done
+done )
+
+# copy android binaries
+cp $IN/native/jbit-android-*.bin $OUT
+
+# make archives
+( cd tmp ; rm -f ../$ID-android-all.zip ; zip ../$ID-android-all.zip -r bin )
+( cd tmp ; tar czf ../$ID-android-all.tar.gz bin )
+
+# cleanup
+rm -rf tmp
