72dfbc38d3523c9d77d2bd12a4715e96e27f1158
Emanuele Fornara <efornara@users.sourceforge.net>
2016-02-06 19:52:21 +0000

    made static runtime optional in mingw

--- native/MinGW.mk
+++ native/MinGW.mk
@@ -6,9 +6,13 @@ include ../Version.defs
 
 CC = i586-mingw32msvc-gcc
 CXX = i586-mingw32msvc-g++
-CFLAGS = -Wall -O2 -fomit-frame-pointer -DJBIT_VERSION=\"${JBIT_VERSION}\"
+CFLAGS = -Wall -Os -fomit-frame-pointer -DJBIT_VERSION=\"${JBIT_VERSION}\"
 CXXFLAGS = $(CFLAGS) -fno-exceptions -fno-rtti
+ifdef MINGW_STATIC_RUNTIME
 LDFLAGS = -s -static-libstdc++ -static-libgcc
+else
+LDFLAGS = -s
+endif
 LIBS =
 
 OBJS = main.o stdout.o cpu.o asm.o symdefs.o
@@ -21,7 +25,7 @@ EXE = jbit.exe
 all: $(EXE)
 
 $(EXE): $(OBJS)
-	$(CXX) $(CXXFLAGS) $(LDFLAGS) -s -o $(EXE) $(OBJS)
+	$(CXX) $(CXXFLAGS) $(LDFLAGS) -o $(EXE) $(OBJS) $(LIBS)
 
 clean:
 	$(RM) *.exe *.o
