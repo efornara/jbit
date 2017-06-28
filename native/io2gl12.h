32fbd238b4a4a8896c5b299a5f0e8df627fe6b10
Emanuele Fornara <efornara@users.sourceforge.net>
2017-06-28 19:56:46 +0200

    io2sim: use linear filtering when upscaling framebuffer

--- native/io2gl12.h
+++ native/io2gl12.h
@@ -41,7 +41,7 @@ static const GLfloat gl12_texcoord[] = {
 struct GL12Renderer {
 	void init() {
 		glEnable(GL_TEXTURE_2D);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 		glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
 		glEnableClientState(GL_VERTEX_ARRAY);
